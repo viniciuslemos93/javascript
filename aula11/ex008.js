cbe79a73324441826be6ea1a4c832c987021c7b9
Vinicius R. Lemos <104476108+viniciuslemos93@users.noreply.github.com>
2022-05-30 14:51:20 -0300

    estruturas condicionais
    estruturas condicionais com JS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro é ${vel}`)
+if (vel > 60){
+    console.log(`A velocidade do seu carro é ${vel}Km/h, você foi MULTADO! `)
+}
+console.log('Dirija sempre com cinto de segurança!')
