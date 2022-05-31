8f73c243f0975fc8698cb630c73e1904de538f7d
Vinicius R. Lemos <104476108+viniciuslemos93@users.noreply.github.com>
2022-05-31 09:15:59 -0300

    estruturas de repeticao
    estrutura básica de while, do while w for

---
+++ aula13-estrutuas-repeticao/ambiente.js
@@ -0,0 +1,19 @@
+/*console.log('Olá')
+console.log('Olá')
+console.log('Olá')
+console.log('Olá')
+*/
+//var cont = 0
+/*while (cont < 6) {
+    console.log('Olá')
+    cont++
+}*/
+/*do {
+    console.log(`Passo ${cont}`)
+    cont++
+}while (cont < 6)*/
+console.log('Vai começar')
+for (var cont = 0; cont <= 6; cont++) {
+    console.log('Olá')
+}
+console.log('FIM!')
